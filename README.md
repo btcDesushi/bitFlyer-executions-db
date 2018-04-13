b8174e046a39edc2a9761f6a05f0d8fbdfc41837
btcDesushi <34061688+btcDesushi@users.noreply.github.com>
2018-04-13 16:05:00 +0900

    first commit
    とりあえず公開

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # bitFlyer-executions-db
+
+bitFlyerから APIを使い、過去の約定履歴を全て取得してmongoDBに保存します。
+
+お約束ですが、本ツールの使用による損失・損害は一切補償いたしません。ご使用は自己責任でお願いします。
+
+## Install
+
+```shell
+$ git clone https://github.com/btcDesushi/reactive-armour-bot
+$ npm install
+```
+
+## Usage
+
+```shell
+$ node index.js
+```
+## Settings
+
+頭の方にある以下の部分をお手元のmongodbの設定に合わせて変更してください
+
+```javascript
+const mongodbUrl = 'mongodb://localhost:27020/bitflyer';
+```
+
+## Issues
+
+ご意見等ありましたら
+https://github.com/btcDesushi/bitFlyer-executions-db/issues
+こちらかtwitter[@asahinoboru_jp](https://twitter.com/asahinoboru_jp)まで。
+プルリクも歓迎します。
+
+詳しい解説等については要望があれば書きます。
+
+
+## Contributor
+[@asahinoboru_jp](https://twitter.com/asahinoboru_jp)
+BTC: 3QsEk7tbNo3JASo7j8s1rCc4JU6kwoZGh9
+MONA: MBo7eyvXeaQdP17p4eGcfGpgJQHNkdVqar
+
+## License
+Code and documentation copyright 2018 by btcDesushi. Code released under the [MIT License](https://github.com/kokushin/node-twatch/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,76 @@
+const bitflyer = require('bitflyer-promise');
+const MongoClient = require('mongodb').MongoClient;
+
+const mongodbUrl = 'mongodb://localhost:27020/bitflyer';
+
+let dataCount = 0 ;
+let apiCount = 1;
+let minimumId = 999999999999;
+
+function sleep(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+}
+
+(async function main() {
+    console.log('MongoDB へ 接続中...');
+    const db = await MongoClient.connect(mongodbUrl).catch( function(err){
+        console.error(err);
+        console.log('接続エラー');
+        process.exit();
+    });
+
+    const collection = db.collection('executions');
+    collection.createIndex({id:1});
+    collection.createIndex({exec_date:1});
+
+    console.log('過去約定データを取得中...');
+    let executions = await bitflyer.executions({product_code: 'FX_BTC_JPY', count: 500}).catch(function(err){
+        console.error(err);
+        console.log('BF API呼び出しエラー');
+        db.close();
+        process.exit();
+    });;
+    console.log(executions.length);
+
+    console.log('過去約定データをDBに追加...');
+    while( executions.length != 0 ) {
+        for(let k in executions) {
+            if( executions[k].id < minimumId) {
+                minimumId = executions[k].id;
+            }
+            if( await collection.count({id: executions[k].id}) == 0 ) {
+                delete executions[k]._id;
+                await collection.insertOne(executions[k]).catch( function(err){
+                    console.error(err);
+                    console.log('過去約定データ追加エラー');
+                    console.log(executions[k]);
+                    db.close();
+                    process.exit();
+                });
+                console.log('database insert ID:' + executions[k].id);
+                dataCount++;
+            } else {
+                console.log('過去約定データ重複スキップ');
+            }
+        }
+        console.log('DB保存データ数：' + dataCount);
+
+        console.log(minimumId);
+        while(true) {
+            executions = await bitflyer.executions({product_code: 'FX_BTC_JPY', count: 500, before: minimumId}).catch(function(err){
+                console.log('BF API呼び出しエラー');
+                sleep(10000);
+            });;
+            break;
+        }
+        apiCount++;
+        console.log('API呼び出し回数：' + apiCount);
+        console.log('取得データ数：' + executions.length);
+        sleep(500);
+    }
+    db.close();
+})();
